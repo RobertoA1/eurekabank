ffa92bb98acb8b99384de1eb58fbf61dbf8c3c24
David <dalucanoni@unitru.edu.pe>
2024-08-03 21:30:37 -0500

    Clase Moneda casi lista v1.0.0.0

--- entidades/Moneda.java
+++ entidades/Moneda.java
@@ -5,9 +5,27 @@ public class Moneda {
     private String codigo;
     private String descripcion;
 
-    public Moneda(){
-        
+    public Moneda(String codigo, String descripcion){
+        this.codigo = codigo;
+        this.descripcion = descripcion;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    
 
 }
