96dc33e2c9e4c4044bc7177d7ffb2aab65aff535
Roberto <duckyroberto@gmail.com>
2024-08-08 20:14:58 -0500

    Se corrigió errores menores en los procedimientos almacenados.

--- scripts/procedimientosAlmacenados/empleado/RemoverEmpleado.sql
+++ scripts/procedimientosAlmacenados/empleado/RemoverEmpleado.sql
@@ -1,3 +1,4 @@
+DELIMITER //
 CREATE DEFINER=`admin`@`%` PROCEDURE `sp_remEmpleado`(IN codigo CHAR(4))
 BEGIN
 	UPDATE empleado SET estado = 0 WHERE emplcodigo = codigo;
--- scripts/procedimientosAlmacenados/empleado/RevisarEstadoEmpleado.sql
+++ scripts/procedimientosAlmacenados/empleado/RevisarEstadoEmpleado.sql
@@ -1,3 +1,4 @@
+DELIMITER //
 CREATE DEFINER=`admin`@`%` PROCEDURE `sp_empleado_obtenerEstado`(IN codigo CHAR(4))
 BEGIN
 	SELECT estado FROM empleado WHERE emplcodigo = codigo;
