65b7e8c58b959e5c928e6ea3b8dfde2ce372873e
Roberto <duckyroberto@gmail.com>
2024-08-09 21:50:32 -0500

    Se arregló problemas en procedimientos almacenados. Se agregó la variable estado y permisos en usuario. Se eliminaron comentarios innecesarios de prueba. Ahora el menú (FrmPrincipal/FrmSecundario) que se abra dependerá del nivel de permisos del usuario.

--- scripts/procedimientosAlmacenados/usuario/sp_insertar_usuario.sql
+++ scripts/procedimientosAlmacenados/usuario/sp_insertar_usuario.sql
@@ -1,5 +1,5 @@
 DELIMITER //
-CREATE DEFINER =`admin`@`%` PROCEDURE `sp_insertar_usuario`(IN u_codigo CHAR(8), u_clave CHAR (56))
+CREATE DEFINER =`admin`@`%` PROCEDURE `sp_insertar_usuario`(IN u_codigo CHAR(8), IN u_clave CHAR(56), IN u_nivelPermisos TINYINT)
 BEGIN
-    insert into usuario values (u_codigo, u_clave);
+    insert into usuario (codigo, clave, permisos) values (u_codigo, u_clave, u_nivelPermisos);
 END
