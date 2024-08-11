e25637ab748525d195f25d3c2229b8a451c4e171
Roberto <duckyroberto@gmail.com>
2024-08-11 17:17:26 -0500

    Se arregló un problema que los administradores accedieran a su menú.

--- src/presentacion/login/Login.java
+++ src/presentacion/login/Login.java
@@ -17,6 +17,7 @@ import javax.swing.JOptionPane;
 
 import java.sql.SQLException;
 import presentacion.*;
+import presentacion.administrador.FrmAdministrador;
 
 import seguridad.Autenticacion;
 
@@ -237,6 +238,11 @@ public class Login extends javax.swing.JFrame {
                 /* Si el nivel de permisos es 10, quiere decir que es un empleado */
                 /* Por ende, lpermilamamos FrmSecundario, que es de empleados */
                 FrmEmpleado menu = FrmEmpleado.getInstance();
+                menu.setVisible(true);
+            }
+            
+            if (user.getNivelPermisos() == 20){
+                FrmAdministrador menu = FrmAdministrador.getInstance();
                 menu.setVisible(true);
             }
             
